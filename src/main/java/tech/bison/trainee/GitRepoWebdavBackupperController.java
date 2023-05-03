1e46611a62f3f3bebd478dca8cb3443a2a0d5ced
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-05-03 20:04:16 +0200

    Add basic authorization functionality

--- src/main/java/tech/bison/trainee/GitRepoWebdavBackupperController.java
+++ src/main/java/tech/bison/trainee/GitRepoWebdavBackupperController.java
@@ -2,6 +2,7 @@ package tech.bison.trainee;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,34 +16,54 @@ import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.sardine.Sardine;
 import com.github.sardine.SardineFactory;
+import com.google.common.hash.Hashing;
 
 @RestController
 public class GitRepoWebdavBackupperController {
 
 	@PostMapping("/backup")
-	public void backupRepo() {
-		String repoUrl = System.getenv("REPO_URL");
-		String localPath = System.getenv("LOCAL_PATH");
-		String username = System.getenv("USERNAME");
-		String password = System.getenv("PASSWORD");
-		String webdavUrl = System.getenv("WEBDAV_URL");
-		String webdavUsername = System.getenv("WEBDAV_USERNAME");
-		String webdavPassword = System.getenv("WEBDAV_PASSWORD");
+	public ResponseEntity<String> backupRepo(@RequestBody String token) {
+		String tokenHash = System.getenv("TOKEN_HASH");
+		if (authorizationIsOK(token, tokenHash)) {
+			String repoUrl = System.getenv("REPO_URL");
+			String localPath = System.getenv("LOCAL_PATH");
+			String username = System.getenv("USERNAME");
+			String password = System.getenv("PASSWORD");
+			String webdavUrl = System.getenv("WEBDAV_URL");
+			String webdavUsername = System.getenv("WEBDAV_USERNAME");
+			String webdavPassword = System.getenv("WEBDAV_PASSWORD");
 
+			return proceedBackup(repoUrl, localPath, username, password, webdavUrl, webdavUsername, webdavPassword);
+		} else {
+			return ResponseEntity.status(401).body("Unauthorized");
+		}
+	}
+
+	private boolean authorizationIsOK(String token, String tokenHash) {
+		return token == null ? false
+				: tokenHash.equals(Hashing.sha256().hashString(token, StandardCharsets.UTF_8).toString());
+	}
+
+	private ResponseEntity<String> proceedBackup(String repoUrl, String localPath, String username, String password,
+			String webdavUrl, String webdavUsername, String webdavPassword) {
 		try {
 			Path repoDirectory = cloneRepository(repoUrl, localPath, username, password);
 			Path repoArchive = archiveRepository(repoDirectory);
 			deleteLocalDirectory(repoDirectory);
 			uploadToWebDav(repoArchive, webdavUrl, webdavUsername, webdavPassword);
 			FileUtils.delete(repoArchive.toFile());
+			return ResponseEntity.status(200).body("Backup created");
 		} catch (GitAPIException | IOException e) {
 			System.err.println("Error cloning repository and uploading to WebDAV: " + e.getMessage());
 			e.printStackTrace();
+			return ResponseEntity.status(500).body("Backup failed:\n" + e.getStackTrace());
 		}
 	}
 

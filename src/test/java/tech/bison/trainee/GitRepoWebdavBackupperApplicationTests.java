39e72eb1d3ac8c08be668f510dfd0e777a475e10
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-05-14 09:35:00 +0200

    Remove unnecessary test case

--- src/test/java/tech/bison/trainee/GitRepoWebdavBackupperApplicationTests.java
+++ src/test/java/tech/bison/trainee/GitRepoWebdavBackupperApplicationTests.java
@@ -1,13 +1,8 @@
 package tech.bison.trainee;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GitRepoWebdavBackupperApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
